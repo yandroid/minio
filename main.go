53b7bea5017ba751dba60541de3723cd51dcca85
Yaniv Haddad <yaniv.haddad@gmail.com>
2021-09-19 15:39:06 +0300

    Update main.go
    changes from minio to yandroid

--- main.go
+++ main.go
@@ -27,10 +27,10 @@ package main // import "github.com/minio/minio"
 import (
 	"os"
 
-	minio "github.com/minio/minio/cmd"
+	minio "github.com/yandroid/minio/cmd"
 
 	// Import gateway
-	_ "github.com/minio/minio/cmd/gateway"
+	_ "github.com/yandroid/minio/cmd/gateway"
 )
 
 func main() {
