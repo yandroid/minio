efab0626c99b5224249f54811035ca238d741c9a
Yaniv Haddad <yaniv.haddad@gmail.com>
2021-09-19 15:48:17 +0300

    Update gateway.go
    change to yandroid repo

--- cmd/gateway/gateway.go
+++ cmd/gateway/gateway.go
@@ -20,19 +20,19 @@ import (
 	// Import all gateways please keep the order
 
 	// NAS
-	_ "github.com/minio/minio/cmd/gateway/nas"
+	_ "github.com/yandroid/minio/cmd/gateway/nas"
 
 	// Azure
-	_ "github.com/minio/minio/cmd/gateway/azure"
+	_ "github.com/yandroid/minio/cmd/gateway/azure"
 
 	// S3
-	_ "github.com/minio/minio/cmd/gateway/s3"
+	_ "github.com/yandroid/minio/cmd/gateway/s3"
 
 	// HDFS
-	_ "github.com/minio/minio/cmd/gateway/hdfs"
+	_ "github.com/yandroid/minio/cmd/gateway/hdfs"
 
 	// GCS (use only if you must, GCS already supports S3 API)
-	_ "github.com/minio/minio/cmd/gateway/gcs"
+	_ "github.com/yandroid/minio/cmd/gateway/gcs"
 	// gateway functionality is frozen, no new gateways are being implemented
 	// or considered for upstream inclusion at this point in time. if needed
 	// please keep a fork of the project.
